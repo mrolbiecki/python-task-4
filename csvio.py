6c2a3fa01c18c08b2e32fb6718aa4850827f682d
Marcin Rolbiecki <marcin@thinkpad.lan>
2024-10-23 20:04:48 +0200

    missing arguments in csvio functions

--- csvio.py
+++ csvio.py
@@ -1,9 +1,9 @@
 import csv
 
-def read_csv():
+def read_csv(path):
     # FIXME
     return [('Model', ['A', ]), ('Wynik', [17, ]), ('Czas', [465, ])]
 
-def write_to_csv(columns):
+def write_to_csv(path, columns):
     # FIXME
     return
