96a7703dcd58dff9678bc7cf6719e727b2118ab1
jullucy <164040898+jullucy@users.noreply.github.com>
2024-10-24 21:14:45 +0200

    Update pathgen.py
    Pierwsza wersja, jeszcze sprawdzę jutro czy się wszystko zgadza.

--- pathgen.py
+++ pathgen.py
@@ -1,21 +1,56 @@
 from pathlib import Path
 
 def generate_paths(months, weekdays, daytimes):
-    # FIXME
-    return [Path('sty/pon/r')]
+    paths = []
+
+    for month in months:
+        for days in weekdays:
+            start_day, end_day = days
+            for time in daytimes:
+                if start_day == end_day:
+                    path = Path(f"{month}/{day}/{time}")
+                    paths.append(path)
+                else:
+                    paths.append(Path(f'{month}/{start_day}/{time_of_day}'))
+                    paths.append(Path(f'{month}/{end_day}/{time_of_day}'))
+
+    return paths
 
 def calculate_total_time(paths):
     print('Calculating total time...')
-    # FIXME using read_csv()
     time = 0
+
+    for path in paths:
+        try:
+            data = csvio.read_csv(path);
+            if (data[0][1] == 'A')
+                time += data[2][1];
+
+        except FileNotFoundError:
+            print(f"File not found: {path}")
+        except KeyError as e:
+            print(f"Missing column in file {path}: {e}")
+        except ValueError as e:
+            print(f"Invalid data in file {path}: {e}")
+            
     print(f"Total time: {time}.")
 
 
 def generate_random_data():
-    # FIXME
-    return []
+    model = random.choice(['A', 'B', 'C'])
+    result = random.randint(0, 1000)
+    time = random.randint(0, 1000)
+
+    return [model, result, f"{time}s"]
+
 
 def create_random_files(paths):
     print('Creating random files...')
-    # FIXME using write_to_csv
+    
+    for path in paths:
+        path.mkdir(parents=True, exist_ok=True)
+
+        data_row = generate_random_data()
+        csvio.write_to_csv(path, data_row)
+    
     print('Files have been created successfully.')
