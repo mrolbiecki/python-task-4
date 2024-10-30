3b0e75db854729b2eb368f5f4914e02af2e9d65b
WingedFoxx <90320028+WingedFoxx@users.noreply.github.com>
2024-10-30 16:48:56 +0100

    done

--- csvio.py
+++ csvio.py
@@ -1,9 +1,29 @@
 import csv
+from pathlib import Path
+
 
 def read_csv(path):
-    # FIXME
-    return [('Model', ['A', ]), ('Wynik', [17, ]), ('Czas', [465, ])]
+    path_file = Path(path)
+    if not path_file.exists():
+        print("Path doesn't exist")
+        return
+
+    data = []
+    with open('plik.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row)
+
+    return data
 
 def write_to_csv(path, columns):
-    # FIXME
+    path_file = Path(path)
+    if not path_file.exists():
+        print("Path doesn't exist")
+        return
+
+    with open(path + '/Data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in columns:
+            writer.writerow(row)
     return
