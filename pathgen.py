ba2c91b658f744c72cc2878a25d533dc6318b4f6
jullucy <164040898+jullucy@users.noreply.github.com>
2024-11-01 21:18:06 +0100

    done

--- pathgen.py
+++ pathgen.py
@@ -2,19 +2,25 @@ from pathlib import Path
 import random
 import csvio
 
+DAYS_OF_WEEK = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+def get_days_in_range(day_range):
+    start_day, end_day = day_range
+    start_index = DAYS_OF_WEEK.index(start_day)
+    end_index = DAYS_OF_WEEK.index(end_day)
+
+    return DAYS_OF_WEEK[start_index:end_index + 1]
+
 def generate_paths(months, weekdays, daytimes):
     paths = []
-    
+
     for month in months:
-        start_day, end_day = weekdays[months.index(month)]
+        days = get_days_in_range(weekdays[months.index(month)])
         
-        for time in daytimes:
-            if start_day == end_day:
-                path = Path(f"{month}/{start_day}/{time}")
+        for day in days:
+            for time in daytimes:
+                path = Path(f"{month}/{day}/{time}")
                 paths.append(path)
-            else:
-                paths.append(Path(f'{month}/{start_day}/{time}'))
-                paths.append(Path(f'{month}/{end_day}/{time}'))
 
     return paths
 
@@ -25,6 +31,7 @@ def calculate_total_time(paths):
     for path in paths:
         try:
             data = csvio.read_csv(path);
+
             if data[0][1] == 'A':
                 time += data[2][1];
 
@@ -34,9 +41,8 @@ def calculate_total_time(paths):
             print(f"Missing column in file {path}: {e}")
         except ValueError as e:
             print(f"Invalid data in file {path}: {e}")
-            
-    print(f"Total time: {time}.")
 
+    print(f"Total time: {time}.")
 
 def generate_random_data():
     model = random.choice(['A', 'B', 'C'])
@@ -45,14 +51,14 @@ def generate_random_data():
 
     return [model, result, f"{time}s"]
 
-
 def create_random_files(paths):
     print('Creating random files...')
-    
+
     for path in paths:
         path.mkdir(parents=True, exist_ok=True)
 
         data_row = generate_random_data()
+        print(data_row)
         csvio.write_to_csv(path, [["Model", "Result", "Time"], data_row])
-    
+
     print('Files have been created successfully.')
