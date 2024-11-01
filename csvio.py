c4b2c89e0bccd191c178feee818f31eed4a54f01
WingedFoxx <90320028+WingedFoxx@users.noreply.github.com>
2024-11-01 19:38:58 +0100

    fixed path and rows

--- csvio.py
+++ csvio.py
@@ -1,29 +1,26 @@
 import csv
 from pathlib import Path
 
-
 def read_csv(path):
-    path_file = Path(path)
-    if not path_file.exists():
+    if not path.exists():
         print("Path doesn't exist")
         return
 
     data = []
-    with open('plik.csv', 'r') as file:
+    with open(path / 'Data.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             data.append(row)
 
     return data
 
-def write_to_csv(path, columns):
-    path_file = Path(path)
-    if not path_file.exists():
+def write_to_csv(path, rows):
+    if not path.exists():
         print("Path doesn't exist")
         return
 
-    with open(path + '/Data.csv', 'w', newline='') as file:
+    with open(path / 'Data.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        for row in columns:
+        for row in rows:
             writer.writerow(row)
     return
